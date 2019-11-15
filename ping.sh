914834bb7bfc0609538a39ad434caec3fc060005
Oliver Leaver-Smith <oliver@leaversmith.com>
2019-11-15 13:47:18 +0000

    Update ping.sh

--- ping.sh
+++ ping.sh
@@ -10,23 +10,27 @@ FILE="${FAIL_DIR}/$(echo ${URL} | tr '/' '-')"
 
 RESULT=$($CURL ${URL})
 case $RESULT in
-	200|301|302)
+	2*|3*)
 		if [ -f ${FILE} ] ; then
 			CREATED=$(stat -r ${FILE} | awk '{print $9}')
 			EPOCH=$(date +%s)
 			AGE=$(echo "$EPOCH - $CREATED" | bc)
 			PRETTY_AGE=$(${BIN_DIR}/displaytime.sh $AGE)
 			rm ${FILE}
-			WEBHOOK_DATA=$(printf "{\"channel\":\"${CHANNEL}\",\"attachments\":[{\"fallback\":\"${URL} back after ${PRETTY_AGE}\",\"text\":\"${URL} back after ${PRETTY_AGE}\",\"color\":\"good\"}]}");
-			echo $WEBHOOK_DATA
-			curl -sX POST "${WEBHOOK_URL}" -d "${WEBHOOK_DATA}" -H "Content-Type: application/json" > /dev/null;
+			WEBHOOK_DATA=$(printf "{\"channel\":\"${CHANNEL}\",\"attachments\":[{\"fallback\":\"${URL} back after ${PRETTY_AGE}\",\"text\":\"${URL} back after ${PRETTY_AGE}\",\"color\":\"good\"}]}")
+			curl -sX POST "${WEBHOOK_URL}" -d "${WEBHOOK_DATA}" -H "Content-Type: application/json" > /dev/null
+			printf "{\"level\":\"NOTICE\",\"target_url\":\"${URL}\",\"status\":\"up\",\"downtime\":\"${AGE}\"}"
 		fi
 		exit 0;;
+	000)
+		printf "{\"level\":\"WARN\",\"target_url\":\"${URL}\",\"status\":\"failed\"}"
+		exit 1;;
 	*)
 		if [ -f ${FILE} ] ; then
 			exit 0
 		fi
 		touch ${FILE};
-		WEBHOOK_DATA=$(printf "{\"channel\":\"${CHANNEL}\",\"attachments\":[{\"fallback\":\"${URL} returning ${RESULT}\",\"text\":\"${URL} returning *${RESULT}*\",\"color\":\"danger\"}]}");
-		curl -sX POST "${WEBHOOK_URL}" -d "${WEBHOOK_DATA}" -H "Content-Type: application/json" > /dev/null;;
+		WEBHOOK_DATA=$(printf "{\"channel\":\"${CHANNEL}\",\"attachments\":[{\"fallback\":\"${URL} returning ${RESULT}\",\"text\":\"${URL} returning *${RESULT}*\",\"color\":\"danger\"}]}")
+		curl -sX POST "${WEBHOOK_URL}" -d "${WEBHOOK_DATA}" -H "Content-Type: application/json" > /dev/null
+		printf "{\"level\":\"NOTICE\",\"target_url\":\"${URL}\",\"status\":\"down\"}";;
 esac
